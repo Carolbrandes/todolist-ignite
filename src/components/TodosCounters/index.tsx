3704ffd61718d363648bd69f0693cd05bbb0f54f
carol <carolina.brandes@corebiz.ag>
2023-12-06 07:29:10 -0300

    TodosCounters => implementation of progress of concluded tasks

--- src/components/TodosCounters/index.tsx
+++ src/components/TodosCounters/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import styles from './styles.module.scss'
+import { useTodo } from '../../hooks/useTodo'
 
 export const TodosCounters = () => {
+  const {todoList} = useTodo()
+
+  const total = todoList.length
+  const totalOfConcluded = todoList.filter(todo => todo.isConcluded).length
+
+
   return (
-    <section className={styles.CountersWrapper}>
+    <section className={styles.CountersWrapper}></section>
         <div className={styles.createTodos}>
-        Tarefas criadas <span>0</span>
+        Tarefas criadas <span>{total}</span>
         </div>
 
         <div className={styles.concludedTodos}>
-        Concluídas <span>0</span>
+        Concluídas <span>{`${totalOfConcluded} de ${total}`}</span>
         </div>
       
     </section>
