f713615d4598158bff954c8976430191eaddc480
carol <carolina.brandes@corebiz.ag>
2023-12-06 07:29:55 -0300

    TodosCounter: error digit close tag section

--- src/components/TodosCounters/index.tsx
+++ src/components/TodosCounters/index.tsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import styles from './styles.module.scss'
-import { useTodo } from '../../hooks/useTodo'
+import React from "react";
+import styles from "./styles.module.scss";
+import { useTodo } from "../../hooks/useTodo";
 
 export const TodosCounters = () => {
-  const {todoList} = useTodo()
+  const { todoList } = useTodo();
 
-  const total = todoList.length
-  const totalOfConcluded = todoList.filter(todo => todo.isConcluded).length
-
+  const total = todoList.length;
+  const totalOfConcluded = todoList.filter((todo) => todo.isConcluded).length;
 
   return (
-    <section className={styles.CountersWrapper}></section>
-        <div className={styles.createTodos}>
+    <section className={styles.CountersWrapper}>
+      <div className={styles.createTodos}>
         Tarefas criadas <span>{total}</span>
-        </div>
+      </div>
 
-        <div className={styles.concludedTodos}>
+      <div className={styles.concludedTodos}>
         Concluídas <span>{`${totalOfConcluded} de ${total}`}</span>
-        </div>
-      
+      </div>
     </section>
-  )
-}
-
-
+  );
+};
